a63611908511355ea1af73401a3c5163c650a17e
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2023-02-09 13:41:00 +0500

    fix

--- _nuxt/static/1675927559/ru/contacts/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675927559",layout:"default",error:a,state:{counter:0,isOpen:false,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fru\u002Fcontacts",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
